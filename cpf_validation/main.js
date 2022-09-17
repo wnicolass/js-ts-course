a8d5e9611cdd9f3ecaa63f4914c787acdc040c2f
wnicolass <nicolaswgl99@gmail.com>
2022-09-17 16:40:44 +0100

    refactor: validation code optimized

--- cpf_validation/main.js
+++ cpf_validation/main.js
@@ -5,39 +5,39 @@ function calculateCPF(cpfSum) {
 }
 
 function validateCalc(calc, arrCPF) {
-  if (calc > 9) {
-    arrCPF.push(0);
-  } else {
-    arrCPF.push(String(calc));
-  }
+  calc > 9 ? arrCPF.push(0) : arrCPF.push(String(calc));
 
   return arrCPF;
 }
 
 function iterateCPF(arrCPF) {
   let control = arrCPF.length + 1;
-  return calculateCPF(
-    arrCPF
-      .map((num) => Number(num) * control--)
-      .reduce((acc, num) => acc + num, 0)
+
+  const calc1 = calculateCPF(
+    arrCPF.reduce((acc, num) => acc + Number(num) * control--, 0)
   );
+
+  arrCPF = validateCalc(calc1, arrCPF);
+  control = arrCPF.length + 1;
+
+  const calc2 = calculateCPF(
+    arrCPF.reduce((acc, num) => acc + Number(num) * control--, 0)
+  );
+
+  arrCPF = validateCalc(calc2, arrCPF);
+
+  return arrCPF;
 }
 
 function validateCPF(cpf) {
   const cleanCPF = cpf.replace(/\D+/g, "");
   let arrCPF = Array.from(cleanCPF).slice(0, 9);
 
-  let cpfCalc = iterateCPF(arrCPF);
-  arrCPF = validateCalc(cpfCalc, arrCPF);
+  const validatedCPF = iterateCPF(arrCPF);
 
-  cpfCalc = iterateCPF(arrCPF);
-  arrCPF = validateCalc(cpfCalc, arrCPF);
-
-  if (cleanCPF === arrCPF.join("")) {
-    console.log("valid cpf");
-  } else {
-    console.log("cpf not valid");
-  }
+  cleanCPF === validatedCPF.join("")
+    ? console.log("valid cpf")
+    : console.log("cpf not valid");
 }
 
 validateCPF(cpf);
