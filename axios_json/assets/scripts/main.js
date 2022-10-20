4ed0b7bac3b315cdea3b2abca41361eb08ce409b
wnicolass <nicolaswgl99@gmail.com>
2022-10-20 21:54:04 +0100

    feat: create table and populate UI :sparkles:

---
+++ axios_json/assets/scripts/main.js
@@ -0,0 +1,31 @@
+function loadElements(data) {
+  const resultEl = document.createElement("div");
+  const section = document.querySelector(".container");
+  section.appendChild(resultEl);
+
+  const tableEl = document.createElement("table");
+
+  for (let person of data) {
+    const tr = document.createElement("tr");
+
+    const td1 = document.createElement("td");
+    td1.innerHTML = person.nome;
+    tr.appendChild(td1);
+
+    const td2 = document.createElement("td");
+    td2.innerHTML = person.idade;
+    tr.appendChild(td2);
+
+    const td3 = document.createElement("td");
+    td3.innerHTML = person.salario;
+    tr.appendChild(td3);
+
+    tableEl.appendChild(tr);
+  }
+
+  resultEl.appendChild(tableEl);
+}
+
+fetch("people.json")
+  .then((res) => res.json())
+  .then((data) => loadElements(data));
